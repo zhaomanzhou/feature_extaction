3318f84a22ef61f37fa8ed6e30f76fd91d65fb13
zhaomanzhou <zmzsstreet@163.com>
2018-11-13 15:47:37 +0800

    Update PictureExtraction.py

--- PictureExtraction.py
+++ PictureExtraction.py
@@ -1,5 +1,6 @@
 from sklearn import datasets
 import matplotlib.pyplot as plt
+#https://scikit-learn.org/stable/auto_examples/datasets/plot_digits_last_image.html#sphx-glr-auto-examples-datasets-plot-digits-last-image-py
 digits = datasets.load_digits()
 print('Digit:', digits.target[0])
 print(digits.images[0])
