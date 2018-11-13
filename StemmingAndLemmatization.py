f6fb4b609d245bea50d96ef4b1eaf666d783e162
zmz <zmzsstreet@163.com>
2018-11-13 23:00:35 +0800

    update

--- StemmingAndLemmatization.py
+++ StemmingAndLemmatization.py
@@ -1,3 +1,8 @@
+#nltk.download('wordnet')
+#nltk.download('punkt')
+
+
+
 # nltk basic usage
 # from nltk.stem.wordnet import WordNetLemmatizer
 # lemmatizer = WordNetLemmatizer()
@@ -18,3 +23,11 @@ corpus = [
 stemmer = PorterStemmer()
 #https://blog.csdn.net/baishuiniyaonulia/article/details/79301092
 print('Stemmed:', [[stemmer.stem(token) for token in word_tokenize(document)] for document in corpus])
+
+def lemmatize(token, tag):
+    if tag[0].lower() in ['n', 'v']:
+        return lemmatizer.lemmatize(token, tag[0].lower())
+    return token
+lemmatizer = WordNetLemmatizer()
+tagged_corpus = [pos_tag(word_tokenize(document)) for document in corpus]
+print ('Lemmatized:', [[lemmatize(token, tag) for token, tag in document] for document in tagged_corpus])
